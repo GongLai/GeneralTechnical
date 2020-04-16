2204f93de094a156953246343e642e771bdb059b
GongLai <gonglaihuang@163.com>
2020-04-16 19:07:03 +0800

    1.0代理池 -- 1.3 实现代理池的日志模块

--- IPProxyPool/settings.py
+++ IPProxyPool/settings.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
+
 
 # 代理IP默认最高分值：50
 MAX_SCORE = 50
+
+# 日志信息的默认配置
+LOG_LEVEL = logging.INFO  # 默认等级
+LOG_FMT = '%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s'  # 默认日志格式
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'  # 默认时间格式
+LOG_FILENAME = 'log.log'  # 默认日志文件名称
+
